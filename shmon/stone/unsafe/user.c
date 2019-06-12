b7654e28a538dca8a9dc4f9d9851b7f75b2ea521
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2019-06-12 11:55:52 +0800

    user_home_dir() is available in macOS

--- shmon/stone/unsafe/user.c
+++ shmon/stone/unsafe/user.c
@@ -1,4 +1,61 @@
+#include <string.h>
 
-int identity(int i) {
-    return i;
+#ifndef __windows__
+#include <pwd.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <errno.h>
+#endif
+
+#ifndef __windows__
+static char* user_fill_passwd_by_name(const char* name, struct passwd* pwd) {
+    struct passwd* result = NULL;
+    char* pool = NULL;
+    long psize = 0L;
+    int retcode = 0;
+    
+    if ((psize = sysconf(_SC_GETPW_R_SIZE_MAX)) == -1) {
+        psize = 1024L;
+    }
+
+    do {
+        if (pool != NULL) {
+            free(pool);
+            psize = psize * 2L;
+        }
+
+        pool = (char*)malloc(psize);
+        retcode = getpwnam_r(name, pwd, pool, psize, &result);
+    } while (retcode == ERANGE);
+
+        
+    if (result == NULL) {
+        free(pool);
+        pool = NULL;
+    }
+
+    return pool;
 }
+#endif
+
+size_t user_home_dir(const char* name, char* pool, size_t psize) {
+    size_t dirsize = 0;
+
+    if (name != NULL) {
+#ifndef __windows__
+        struct passwd pwd;
+        char* buffer = user_fill_passwd_by_name(name, &pwd);
+        
+        if (buffer != NULL) {
+            dirsize = strnlen(pwd.pw_dir, psize);
+            strncpy(pool, pwd.pw_dir, dirsize);
+
+            free(buffer);
+        }
+#endif
+    }
+
+    return dirsize;
+}
+
--- shmon/stone/unsafe/user.rkt
+++ shmon/stone/unsafe/user.rkt
@@ -6,6 +6,16 @@
 
 (define-ffi-definer define-user user-lib)
 
-(define-user identity (_fun _int -> _int))
+(define-user user_home_dir
+  (_fun #:save-errno 'posix
+        [name : _symbol]
+        [buffer : _bytes = (make-bytes 1024)]
+        [bsize : _size = (bytes-length buffer)]
+        -> [dirsize : _ulong]
+        -> (and (> dirsize 0)
+                (bytes->path (cond [(= dirsize bsize) buffer]
+                                   [else (subbytes buffer 0 dirsize)])))))
 
-(identity 12)
+(user_home_dir 'wargrey)
+(user_home_dir 'root)
+(user_home_dir 'tamer)
