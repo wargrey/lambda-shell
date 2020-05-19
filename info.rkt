5674dfdd0398c07bb38dbab1ed09a50caf6fc2b3
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2020-05-19 23:28:09 +0800

    fix dependencies

--- info.rkt
+++ info.rkt
@@ -5,5 +5,5 @@
 (define pkg-authors '(wargrey))
 (define version "1.0")
 
-(define deps '("base" "digimon" "typed-racket-lib" "typed-racket-more"))
-(define build-deps '("digimon" "scribble-lib" "racket-doc"))
+(define deps '("base" "digimon" "typed-racket-lib" "typed-racket-more" "scribble-lib" "pict-lib" "math-lib"))
+(define build-deps '("digimon" "scribble-lib" "pict-lib" "math-lib" "racket-doc"))
