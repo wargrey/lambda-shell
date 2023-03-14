24265ce1a72d84b423877edc5080abc1e3575c37
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2023-03-14 10:23:03 +0800

    sync with `digimon/exception`

--- ssh/digitama/fsio/exception.rkt
+++ ssh/digitama/fsio/exception.rkt
@@ -6,10 +6,13 @@
 
 (require digimon/exception)
 
-(define-exception exn:ssh:fsio exn:fail:filesystem () (ssh-exn-fsio-message [/dev/stdin : Input-Port] [line : (Option Natural)] [col : (Option Natural)]))
+(define-exception exn:ssh:fsio exn:fail:filesystem ()
+  (ssh-exn-fsio-message [/dev/stdin : Input-Port] [line : (Option Natural)] [col : (Option Natural)]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define ssh-exn-fsio-message : (-> Any Input-Port (Option Natural) (Option Natural) String String)
+(define ssh-exn-fsio-message : (-> Any Input-Port (Option Natural) (Option Natural) String (Values String (Option Continuation-Mark-Set)))
   (lambda [func /dev/stdin line col msg]
-    (cond [(and line col) (ssh-exn-message func (string-append (format "~a:~a:~a: " (object-name /dev/stdin) line col) msg))]
-          [else (ssh-exn-message func (string-append (format "~a: " (object-name /dev/stdin)) msg))])))
+    (values (if (and line col)
+                (ssh-exn-message func (string-append (format "~a:~a:~a: " (object-name /dev/stdin) line col) msg))
+                (ssh-exn-message func (string-append (format "~a: " (object-name /dev/stdin)) msg)))
+            #false)))
